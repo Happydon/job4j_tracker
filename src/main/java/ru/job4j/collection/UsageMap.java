688040ed1e5b7ee8171b53727f2da076e7d0e7a5
Happydon <ilijadavincy@gmail.com>
2020-10-19 15:01:00 +0300

    0. Map, HashMap [#263943]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("bestJedi@yandex.ru", "Obi Wan Kenobi");
+        map.put("KolBask@mail.ru","Nikolay Olegovich Baskov");
+        for (String key : map.keySet()){
+            String value = map.get(key);
+            System.out.println(key + " " + value);
+        }
+    }
+}
