94f9a7b47b2e0ca154300ef7f07b347258f32ef0
Happydon <ilijadavincy@gmail.com>
2020-10-29 17:56:30 +0300

    Отсортировать департаменты [#263926]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        LinkedHashSet<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start = start.isBlank() ? el : start + "/" + el;
+                tmp.add(start);
+            }
+        }
+        List<String> list = new ArrayList<>(tmp);
+        sortAsc(list);
+        return list;
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+}
